5b195db6f503bc5dbc8c9fdb7cfcfef80390e2f2
Deirdre O'Regan <deirdre.oregan@gmail.com>
2015-08-07 14:41:22 +0200

    Changing test

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var request = require('supertest');
 var app = require('../app.js');
 
 describe('GET /', function() {
-  it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello world', done);
+  it('respond with index page', function(done) {
+    request(app).get('/').expect(200, done);
   });
 });
