0c56a062eb6c20b0ca773f28f639eb1f788b2079
Deirdre O'Regan <deirdre.oregan@gmail.com>
2015-08-07 14:05:42 +0200

    Addig Front-End Stuff

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express();
+app.use('/', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.render('index.html')
 });
 
 app.listen(process.env.PORT || 5000);
---
+++ public/config.rb
@@ -0,0 +1,5 @@
+require "susy"
+
+http_path = '/'
+css_dir = "css"
+sass_dir = "sass"
